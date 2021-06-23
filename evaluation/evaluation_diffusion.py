f9a2724fab3906be7e2fc7cd19137fb451f1841f
lisa-schroeder <uqlzo@student.kit.edu>
2021-06-23 16:13:58 +0200

    evaluate diffusion coefficient
    insert csv with description in header

--- evaluation/evaluation_diffusion.py
+++ evaluation/evaluation_diffusion.py
@@ -11,9 +11,13 @@ gradient_length = 0.004     # sec, D71
 diffusion_time = 0.025      # sec, D74
 maximum_gradient = 0.2977       # T*m^(-1)
 relative_gradient = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-
+#columns = ["triplet", "quartet", "acide H", "H2O", "acide H + H2O"]
+columns = pd.read_csv("values.csv", header = None)
+#print(columns2.loc[0,1])
 # import values to evaluate
-values = pd.read_csv("values.csv", sep=",")
+values = pd.read_csv("values.csv", sep=",", header = None)
+
+# print(values)
 
 # print(values)
 # print(values.loc[1, "triplet"])
@@ -26,41 +30,35 @@ def find_D(values):
         # formula from excel sheet
         x[i] = 2*(gyromagn_ratio**2)*((gradient_length*(2/math.pi))**2)*((maximum_gradient*relative_gradient[i])**2)*(diffusion_time/2-(gradient_length*(2/math.pi))/3)*(10**(-9))
     x = x.reshape((-1, 1))
-    print(x)
+    # print(x)
 
-    y_trip = []
-    # calculate the values of the y-axis
-    for i in range(11):
-        y_trip = y_trip + [values.loc[i, "triplet"]]
-    # print(y_trip)
-    y_quart = []
-    for i in range(11):
-        y_quart = y_quart + [values.loc[i, "quartet"]]
-    # print(y_quart)
-    y_trip_fin = []
-    y_quart_fin = []
+    D = []
+    print(values)
 
+    for i in range(6-1):
 
-    for i in range(len(y_trip)-1):
-        y_trip_fin = y_trip_fin + [ln(y_trip[i+1]/y_trip[0])]
-    print(y_trip_fin)
+        y_raw = []
+        # calculate the values of the y-axis
+        for j in range(11):
+            y_raw = y_raw + [float(values.loc[j+1, i])]
+        # print(y_raw)
+        y = []
 
-    for i in range(len(y_quart)-1):
-        y_quart_fin = y_quart_fin + [ln(y_quart[i+1]/y_quart[0])]
-    # print(y_quart_fin)
-    # fit
+        for j in range(len(y_raw)-1):
+            y = y + [ln(y_raw[j+1]/y_raw[0])]
+        # print(y)
 
-    trip = LinearRegression().fit(x, y_trip_fin)
-    print("r_sq: ", trip.score(x, y_trip_fin))
-    print("intercept: ", trip.intercept_)
-    print("slope: ", trip.coef_[0])
+       # fit
+        model = LinearRegression().fit(x, y)
+        #print("r_sq: ", model.score(x, y))
+        #print("intercept: ", model.intercept_)
+        #print("slope: ", model.coef_[0])
 
-    quart = LinearRegression().fit(x, y_quart_fin)
-    print("r_sq: ", quart.score(x, y_quart_fin))
-    print("intercept: ", quart.intercept_)
-    print("slope: ", quart.coef_[0])
+        D = D + [[columns.loc[0, i], -model.coef_[0]]]
 
+    return(D)
 
-    return(trip.coef_[0], quart.coef_[0])
-
-print(find_D(values))
+D = find_D(values)
+data_frame = pd.DataFrame(D)
+data_frame.to_csv("diffusion_coefficient.csv")
+# pd.concat([values, data_frame]).to_csv("Values3.csv")
